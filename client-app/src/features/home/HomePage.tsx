7287081034ce9cf877e45bbf2cd4982433f1e84a
John alexander ubaque laguna <jubaque@sb.gob.do>
2021-05-06 07:03:33 -0500

    se agregan estilos

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,12 +1,17 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
-import { Container } from 'semantic-ui-react'
+import { Container, Header, Segment,Image, Button } from 'semantic-ui-react'
 
 export default function HomePage() {
     return (
-      <Container style={{marginTop:'7em'}}>
-          <h1>Home Page</h1>
-          <h3> goto <Link to='/ctivities'> activities</Link></h3>
+    <Segment inverted textAlign='center' vertical className ='masthead'>
+      <Container text >
+          <Header  as='h1' inverted>
+              <Image size ='massive' src='/assets/logo.png' alt='logo' style ={{marginBottom:12}}/>
+              Reactivities
+          </Header>
+          <Header as ='h2' inverted content='Welcome to Reactivities'/>
+          <Button as ={Link} to ='/activities' size='huge' content ='take to the activities '/>
       </Container>
+    </Segment>
     )
 }
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import 'react-calendar/dist/Calendar.css'
 import 'semantic-ui-css/semantic.min.css'
 import App from './app/layout/App';
 import './app/layout/styles.css';
